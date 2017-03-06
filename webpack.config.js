2ee6ed0785d68867c4fc9d64d638190b09adadc6
David Conner <rdconner@us.ibm.com>
2017-03-06 15:13:21 -0600

    adding forgotten eslint loader

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,12 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
